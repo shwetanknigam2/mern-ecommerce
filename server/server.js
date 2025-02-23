bf1be1b9b81476c926aa612b1f3b5b54937433f3
Shwetank <shwetanknigam2@gmail.com>
2025-02-23 17:13:04 +0530

    try it once

--- server/server.js
+++ server/server.js
@@ -24,7 +24,8 @@ const PORT=process.env.PORT||5001
 
 app.use(
     cors({
-        origin: 'https://mern-ecommerce-1-j6ok.onrender.com',
+        // origin: 'https://mern-ecommerce-1-j6ok.onrender.com',
+        origin: '*',
         methods:['GET','POST','DELETE','PUT'],
         allowedHeaders:[
             "Content-Type",
